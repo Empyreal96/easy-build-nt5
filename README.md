eeb3a8c7f44bc532304b308bbefcf468f0d80793
Empyreal96 <64583248+Empyreal96@users.noreply.github.com>
2020-11-03 03:20:09 +0000

    Updated README and Script options
    - Build (dirty and clean) works
    - prebuild works
    - Build info shown in main menu

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# easy-build-nt5
+# Easy-Build-Env
 Just a small script I made and use when building NT5 for a quick lazy mans build. Adapted so others can use
+
+
+
+- menu.cmd - To use, copy to srv03rtm(or xpsp1)\tools, run razzle, and type menu
+
+  
--- menu.cmd
+++ menu.cmd
@@ -1,4 +1,5 @@
 @echo off
+cd %_NTROOT%
 cls
 echo I take no credit for finding the build commands and methods used, like many I followed
 echo the development threads on 4chan/g/wxp, so all credit goes to the /wxp Anons!
@@ -8,6 +9,7 @@ timeout /T 8
 goto :mainmenu
 
 :mainmenu
+cd %_NTROOT%
 cls
 echo Here you will be able to run basic prebuild, build and postbuild scripts
 echo Current build User: %_NTUSER%
@@ -28,22 +30,29 @@ echo 4) Create ISO image
 echo 5) Exit
 echo.
 set /p NTMMENU=Select:
-if "%NTMMENU%"=="1" build -bcZP
+if "%NTMMENU%"=="1" goto CleanBuild
 if "%NTMMENU%"=="2" build -bZP
 if "%NTMMENU%"=="b" call :BuildError
 if "%NTMMENU%"=="3" postbuild.cmd
 if "%NTMMENU%"=="p" call :PostError
 if "%NTMMENU%"=="4" call :MakeISO
-if "%NTMMENU%"=="5" exit
-if "%NTMMENU%"=="pre" goto :PreEnv
+if "%NTMMENU%"=="5" exit /b
+if "%NTMMENU%"=="pre" cmd /c prebuild.cmd&& timeout /t 3 && goto mainmenu
 if "%NTMMENU%"=="var" set
 goto mainmenu
 
 :PreEnv
-cmd /c prebuild.cmd&& goto mainmenu
+
 
 :CleanBuild
+cd /d %~d0\srv03rtm
+timeout /t 3
+cmd /k build -bcZP&& pause && goto mainmenu
+
 :DirtyBuild
+cd /d %~d0\srv03rtm
+timeout /t 3
+cmd /k build -bZP&& pause && goto mainmenu
 :BuildError
 :PostError
 :MakeISO
