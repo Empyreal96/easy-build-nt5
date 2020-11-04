f6eb4ec1a710d94d9a6e8809db3767b93b7f5be6
Empyreal96 <64583248+Empyreal96@users.noreply.github.com>
2020-11-04 15:23:00 +0000

    Stability improved, checks added
    - 64 bit env setup works
    - Checks on if missing files present
    - Checks if razzle present
    - info screen
    -

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # Easy-Build GUI For Razzle
 Just a small script I made and use when building NT5 for a quick lazy mans build. Adapted so others can use.
 
-![MainMenu](https://github.com/Empyreal96/easy-build-nt5/raw/main/easy-build.png)
+![](https://github.com/Empyreal96/easy-build-nt5/raw/main/easy-build.png)
+
+
+
+```
+**What's new?**
+	- Added checks so that the script cannot run without razzle present
+	- Added checks so user cannot postbuild or make ISO with files missing (missing files not provided)
+	- Added 'First Run' screen (it will only show on very first load)
+	- Made sure we can load 64 bit env successfully
+	- Added an 'info' page 
+	- All options should work now
+```
+
+
 
 - **easy-build.cmd:**
 
--- easy-build.cmd
+++ easy-build.cmd
@@ -1,8 +1,10 @@
 @echo off
 color 09
+REM
+REM UAC elevation
 REM I found this method to elevate the script to admin here: https://stackoverflow.com/a/12264592
 REM I chose it because it is what my Windows 10 Toolbox uses, and works well!
-
+REM
 :init
  setlocal DisableDelayedExpansion
  set cmdInvoke=1
@@ -45,14 +47,77 @@ REM I chose it because it is what my Windows 10 Toolbox uses, and works well!
 :gotPrivileges
  setlocal & cd /d %~dp0
  if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
-REM End UAC elevation
+goto RazCheck
+ REM END UAC ELEVATION
+:RazCheck
+if exist %~dp0\razzle.cmd (goto easyfirstcheck) else (goto WhereisRaz)
 
-
+:WhereisRaz
+REM
+REM This screen shows when 'razzle.cmd' cannot be found.
+REM we decide this by the above sectiin :RazChech which simply
+REM sees if the file is present in the working directory of this file.
+REM If not found we redirect to this section which throws up a 'Bluescreen'
+REM With host OS Version and bogus error number.. for shits and giggles lol
+REM
+cls
+color 17
+ver
+echo.
+echo Easy-Build has encountered an error and needs to close.
+echo Application failed to start - 0x001a
+echo.
+echo RAZZLE_ENVIRONMENT_NOT_FOUND
+echo.
+echo If this is the first time seeing this, close this Window, copy Easy-Build
+echo to the same directory as 'razzle.cmd' and try again.
+echo.
+pause
+exit
+REM goto easyinit
+:easyfirstcheck
+REM
+REM This is a check to see if 'first run' has been completed, the .txt file 
+REM is created after the first run dialogue
+REM
+if exist %~dp0\easy-build-check.txt (goto easyinit) else (goto easyfirstrun)
+REM
+REM This is the 'first run' dialogue, self explanitory. Creates the 'first run' complete
+REM txt after this windows, It also checks for said .txt upon loading the dialogue and skips
+REM this section if it is found.
+RRM
+:easyfirstrun
+if exist %~dp0\easy-build-check.txt (goto easyinit)
+cls
+echo --------------------------------------------------------------------------------------------
+echo Welcome to Easy-Build! (This will only show once)
+echo.
+echo This tool is designed for those messing with the NT5 source, but would like a quick tool to 
+echo build through to ISO creation. It also shows info on current build env!
+echo.
+echo This tool will automatically load Razzle32/64 environment and ask for UAC access. 
+echo Just create a Desktop Shortcut, and Double Click.
+echo Over time I will add more capabilities to what this tool can do.
+echo --------------------------------------------------------------------------------------------
+echo REQUIREMENTS
+echo.
+echo - 4chan's prepatched source files and missing RTM ISO files.
+echo - Loads of free Disk Space.
+echo - Ideally to have already followed the guide to prep the source.
+echo.
+timeout /t 10 
+REM this echo to file is used to tell if Easy-Build has done it's 'first run'
+echo EASY_BUILD_FIRST_RUN=1 > %~dp0\easy-build-check.txt
+goto easyinit
+:easyinit
+REM
 REM This piece tells the script what Build Number to show depending on the name of NT source dir
 REM It also checks to see if NTROOT has been set as to tell if it needs to start Razzle or not.
+REM
 if "%_NTROOT%" == "\srv03rtm" set _Build_No=3790
 if "%_NTROOT%" == "\XPSP1" set _Build_No=2600
 if "%_NTROOT%" == "" (goto RazzleFirst) else (goto mainmenu)
+REM
 REM =================================================================================
 REM Feel free to modify this script, I just made it to make life lazy for those
 REM needing to build frequently. 
@@ -93,15 +158,17 @@ REM This section simply detects if the users PC is 32 bit or 64 bit;
 REM All it does is look in C:\Windows\ for the SysWOW64 folder.
 REM If the folder exists, it will load the razzle64.cmd (See :Razzle64)
 REM otherwise it will load standard razzle.cmd (See :Razzle32)
+REM
 :RazzleFirst
 echo.
 echo This script needs to run in Razzle.
 echo Starting Razzle32/64 Environment..
 timeout /t 3 /nobreak
 if exist %WinDir%\SysWOW64 (goto Razzle64) else (goto Razzle32)
-
+REM
 REM These are simply 'loaders' for 32 bit and 64 bit razzle;
 REM It uses cmd to (1) execute Razzle, (2) change to the root Build folder and (3) run this file again inside razzle.
+REM
 :Razzle32
 cls
 echo Loading Razzle for 32bit Windows
@@ -114,10 +181,10 @@ cls
 echo Loading Razzle for 64bit Windows
 cmd.exe /k ".\tools\razzle64.cmd free offline && easy-build.cmd"
 pause
-
-
+REM
 REM Menu, pretty self explanitory on the purpose, echo loads of shit with handy info.
 REM I have left the 'var' command in because I use it to see what variables are set
+REM
 :mainmenu
 cd %_NTROOT%
 cls
@@ -141,10 +208,10 @@ echo  1) Clean Build (Full err path, delete object files, no checks)
 echo  2) 'Dirty' Build (Full err path, no checks)
 echo  b) Open build.err in Notepad
 echo --------------------------------------------------------------------------------------------
+echo - DON'T FORGET TO COPY MISSING.7Z CONTENTS
+echo --------------------------------------------------------------------------------------------
 echo  3) Start Postbuild
 echo  p) Open postbuild.cmd's build.err
-echo --------------------------------------------------------------------------------------------
-echo - DON'T FORGET TO COPY MISSING.7Z CONTENTS
 echo --------------------------------------------------------------------------------------------
 echo  4) Create ISO image
 echo  5) Drop to Razzle Prompt
@@ -156,21 +223,20 @@ if "%NTMMENU%"=="2" goto DirtyBuild
 REM Opens the most recent builds error logs in Notepad
 if "%NTMMENU%"=="b" notepad %~d0%_NTROOT%\build.err & goto mainmenu
 REM This starts the postbuild process, see postbuild.cmd for info
-if "%NTMMENU%"=="3" cmd /c postbuild.cmd&& timeout /t 5 && goto mainmenu
+if "%NTMMENU%"=="3" if exist %_NTPOSTBLD%\PIDGEN.DLL (cmd /c postbuild.cmd&& timeout /t 5 && goto mainmenu) else (cls && echo Missing RTM ISO files not present in %~d0\binaries.x86fre && timeout /t 10 && goto mainmenu)
 REM Opens the most recent postbuild error logs in Notepad
 if "%NTMMENU%"=="p" notepad %_NTPOSTBLD%\build_logs\postbuild.err & goto mainmenu
 if "%NTMMENU%"=="4" goto MakeISOCheck
 if "%NTMMENU%"=="5" exit /b
 REM This runs the prebuild.cmd to set up the build environment
 REM If user gets error of file missing, they obviously haven't patched the source with the 4chan/OpenXP files.
-if "%NTMMENU%"=="pre" cmd /c prebuild.cmd&& timeout /t 5 && goto mainmenu
+if "%NTMMENU%"=="pre" cmd /c prebuild.cmd&& pause && goto mainmenu
 if "%NTMMENU%"=="var" set 
 REM Info page on what is set e.g DDK, NT build version and other bits
 if "%NTMMENU%"=="info" goto BuildInfo
 goto mainmenu
 REM 
-
-REM This I have found tempermental, most likely me missing something simple..
+REM Seems to work for me now this bit..
 REM This is where we init the Clean Building process;
 REM (1) cd to current NT root, usually /srv03rtm or /XPSP1, just to be safe
 REM (2) A little pause incase the user fucked up
@@ -186,8 +252,7 @@ timeout /t 3 /nobreak
 cmd /c build -bcZP
 pause
 goto mainmenu
-
-REM Same as before I found troublesome..
+REM 
 REM This is where we init the 'Dirty' Building process, only difference is no deleting objects
 REM (1) cd to current NT root, usually /srv03rtm or /XPSP1, just to be safe
 REM (2) A little pause incase the user fucked up
@@ -195,38 +260,68 @@ REM (3) Use cmd to call the build command, which does:
 REM		-b: Display full error message text
 REM		-Z: No Dependancy checking of source files
 REM		-P: Print time after each directory
-
+REM
 :DirtyBuild
 cd /d %~d0%_NTROOT%
 timeout /t 3 /nobreak
 cmd /c build -bZP
 pause
 goto mainmenu
-
+REM
 REM This checks the Postbuild directory for PIDGEN.DLL from the missing files found at the Anons rentry guide
 REM If the file doesn't exist, we presume the user has NOT copied the required missing files and returns them back
+REM
 :MakeISOCheck
-if exist %_NTPOSTBLD%\PIDGEN.DLL (goto MakeISO) else (cls && echo Missing.7z files not present in Postbuild dir && timeout /t 10 && goto mainmenu)
-
+if exist %_NTPOSTBLD%\PIDGEN.DLL (goto MakeISO) else (cls && echo Missing RTM ISO files not present in %~d0\binaries.x86fre && timeout /t 10 && goto mainmenu)
+REM
 REM This section calls 'oscdimg.cmd' and asks for users input on desired SKU
 REM Not much to say here, it just requires the switch for running oscdimg from razzle
+REM
 :MakeISO
 cls
 echo Please enter one of the SKUs from below to build your ISO (e.g pro)
+echo Type: Key  to view needed Product Key
+echo --------------------------------------------------------------------
 echo.
-echo `srv` - Windows Server 2003 Standard Edition
-echo `sbs` - Windows Server 2003 Small Business Edition
-echo `ads` - Windows Server 2003 Enterprise Edition
-echo `dtc` - Windows Server 2003 Datacenter Edition
-echo `bla` - Windows Server 2003 Web Edition
-echo `per` - Windows XP Home Edition
-echo `pro` - Windows XP Professional
+echo  srv) Windows Server 2003 Standard Edition
+echo  sbs) Windows Server 2003 Small Business Edition
+echo  ads) Windows Server 2003 Enterprise Edition
+echo  dtc) Windows Server 2003 Datacenter Edition
+echo  bla) Windows Server 2003 Web Edition
+echo  per) Windows XP Home Edition
+echo  pro) Windows XP Professional
+echo Back)
 echo.
-set /p oscd=
+set /p oscd=Select:
+if "%oscd%"=="key" goto ProdKeys
 cmd /c oscdimg.cmd %oscd%&& timeout /t 5
 goto mainmenu
 
+:ProdKeys
+REM
+REM These are they keys shown in the 4chan Rentry guide
+REM Put them here for ease
+REM
+REM
+cls
+echo  Here are the Keys used during Installaion for each SKU
+echo.
+echo           - SRV, SBS, DTC, BLA, PER, PRO: -
+ECHO           - M6RJ9-TBJH3-9DDXM-4VX9Q-K8M8M -
+ECHO.
+echo           -            ADS Only:          -
+echo           - QW32K-48T2T-3D2PJ-DXBWY-C6WRJ -
+echo.
+pause
+goto MakeISO
 :BuildInfo
+REM
+REM This info page pulls the variables set by Razzle and some source files and
+REM creates readable outputs to the user
+REM 
+REM I'm not going to explain each var as they should be rather obvious reading them
+REM But I will add more here at a later date
+REM
 cls
 if "%_NO_DDK%" == "1" (set ddk_info=DDK Building Off) else (set ddk_info=DDK Building Off)
 if "%_BuildOpt%" == "full opt" (set bldopt_info=Full Optimization) else (set bldopt_info=No Optimization)
@@ -235,11 +330,11 @@ if "%BUILD_PRODUCT_VER%" == "500" (set ntbld_ver=Targeted NT5) else (set ntbld_v
 if "%BUILD_MULTIPROCESSOR%" == "1" (set buildthreads_info=Multithreaded Building) else (set buildthreads_info=Singlethreaded Building)
 set "prerel_read="
 for /F "skip=13 delims=" %%i in (%~d0%_NTROOT%\base\prerelease.inc) do if not defined prerel_read set "prerel_read=%%i"
-if "%prerel_read%" == "PRERELEASE=0" (set prerel_info=Prerelease Build) else (set prerel_info=Retail Build)
+if "%prerel_read%" == "PRERELEASE=0" (set prerel_info=Retail Build) else (set prerel_info=Prerelease Build)
 echo ----------------------------------------------------------------
 echo Various information about current build environment
 echo ----------------------------------------------------------------
-echo Build Options and Info
+echo Build Options and Info:
 echo %ntbld_ver%
 echo %prerel_info%
 echo %bldoffline%          	
@@ -247,7 +342,7 @@ echo %bldopt_info%
 echo %ddk_info%
 echo %buildthreads_info%
 echo.
-echo Other Info
+echo Other Info:
 echo URT Version: %URT_VERSION%
 echo Build Profile: %init%
 echo ----------------------------------------------------------------
